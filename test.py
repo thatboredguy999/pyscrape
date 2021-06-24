284e8ff334325a31efb482cdc2c49b46a2799fe6
root <root@DESKTOP-RRV5919.localdomain>
2021-06-24 12:47:46 -0500

    H_2021_06_22.pdf 	H_2021_06_22.txt 	__pycache__/ 	blank.csv 	blank.txt 	data.txt 	data2.pdf 	data2.txt 	datadump.txt 	datadump2.py 	datadump2.txt 	dd2.py 	gs.xml 	miner_text_generator.py 	sample.csv 	sample.txt 	sample.xml 	testminer.py 	xml_exporter.py

--- test.py
+++ test.py
@@ -3,14 +3,21 @@ from PyPDF2 import PdfFileReader
 
 def text_extractor(path):
 	with open(path, 'rb') as f:
+		myText = open(r'blank.csv','w')
 		pdf = PdfFileReader(f)
+		numPage = pdf.getNumPages()
+		count = 0
 		
-		page = pdf.getPage(1)
-		print(page)
-		text = page.extractText()
-		print(text)
+		for x in range (count,numPage):
+			page = pdf.getPage(count)
+			print(page)
+			text = page.extractText()
+			count +=1
+			myText.write(text)
+			print(text)
+		myText.close()
 
 if __name__ == '__main__':
-	path = 'example.pdf'
+	path = 'H_2021_06_22.pdf'
 	text_extractor(path)
 	
