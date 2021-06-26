592188aeae9a771ca3f664a59683f3854ead5f97
root <root@DESKTOP-RRV5919.localdomain>
2021-06-26 04:14:40 -0500

    modified:   app.py 	modified:   dd2.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+import psycopg2, tempfile, subprocess
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
@@ -17,6 +17,93 @@ def validate_image(stream):
 	if not format:
 		return None
 	return '.' + (format)
+
+
+def insert_data(name):
+
+        sql = """INSERT INTO new(PhyDate, Code, ProdDate, FSPrice)
+                VALUES(%s);"""
+        filename, file_extension = os.path.splitext(name)
+        command =('CREATE TABLE {tab} (PhyDate, Code, ProdDate, FSPrice)')
+
+
+        conn = psycopg2.connect(
+                host="localhost",
+                database="sample",
+                user="postgres",
+                password="root")
+        cur = conn.cursor()
+        cur.execute('''DROP TABLE IF EXISTS "{tab}";'''.format(tab=filename))
+        cur.execute('''CREATE TABLE "{tab}" (PhyDate char(50) PRIMARY KEY, Code char(50) SECONDARY KEY, ProdDate char(50) SECONDARY KEY, FSPrice char(50) NOT NULL);'''.format(tab=filename))
+        with open(name , 'r') as f:
+
+                #print('PostgreSQL database version:')
+                cur.copy_from(f, filename, sep=' ')
+        conn.commit()
+        cur.close()
+        if conn is not None:
+                conn.close()
+                print('Database connection close.')
+
+
+def filelayout(filename):
+        newfile=filename.strip('.pdf')
+        newfile2=newfile
+        add_string='.txt'
+        newfile = "".join((newfile, add_string))
+        filepath='uploads/{}'.format(filename)
+#        subprocess.call(["pdftotext", "-layout", filename, 'data.txt'])
+        out, err = subprocess.Popen(["pdftotext", "-layout", filepath, 'data.txt']).communicate()
+        addstring='_.txt'
+        newfile2="".join((newfile2, addstring))
+
+        with open('data.txt','r') as file, open(newfile2,'w') as destination:
+                check=False
+                count=0
+                counton=False
+                hold= ' '
+                test=True
+                broken=False
+                for line in file:
+                        for word in line.split():
+                                if word == 'H':
+                                        check=True
+                                        counton=True
+                                        destination.write('6/25/2021 '+ word+ ' ')
+                                        print(word)
+                                elif count==2:
+                                        hold=word
+
+
+                                elif check==True :
+                                        check=False
+                                        destination.write(word + ' ')
+                                        print(word)
+                                elif count==5 :
+                                        if ',' in word:
+                                                broken=True
+
+                                elif count==6 :
+                                        if '0' in word:
+                                                broken=True
+
+                                        if broken==True:
+                                                counton=False
+                                                count=0
+                                                destination.write(hold + '\n')
+                                                print(hold)
+                                                broken=False
+                                        else:
+                                                print(word)
+                                                destination.write(word + '\n')
+                                                counton=False
+                                                count=0
+                                if counton==True:
+                                        count +=1
+
+
+
+
 
 
 @app.route('/')
@@ -39,6 +126,8 @@ def upload_file():
                 if file_ext not in app.config['UPLOAD_EXTENSIONS']: # file_ext != validate_image(uploaded_file.stream):
                        abort(400)
                 uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'],filename))
+
+        filelayout(filename)
         return redirect(url_for('index'))
 
 
--- dd2.py
+++ dd2.py
@@ -4,49 +4,49 @@ def filelayout():
 	out, err = subprocess.Popen(["pdftotext", "-layout", 'H_2021_06_22.pdf','H_2021_06_22.txt']).communicate()
 
 
-with open('data2.txt','r') as file, open('hold.txt','w') as destination:
-	check=False
-	count=0
-	counton=False
-	hold= ' '
-	test=True
-	broken=False
-	for line in file:
-		for word in line.split():
-			if word == 'H':
-				check=True
-				counton=True
-				destination.write('6/25/2021 '+ word+ ' ')
-				print(word)
-			elif count==2:
-				hold=word
+	with open('data2.txt','r') as file, open('hold.txt','w') as destination:
+		check=False
+		count=0
+		counton=False
+		hold= ' '
+		test=True
+		broken=False
+		for line in file:
+			for word in line.split():
+				if word == 'H':
+					check=True
+					counton=True
+					destination.write('6/25/2021 '+ word+ ' ')
+					print(word)
+				elif count==2:
+					hold=word
 				
 
-			elif check==True :
-				check=False
-				destination.write(word + ' ')
-				print(word)
-			elif count==5 :
-				if ',' in word:
-					broken=True
-
-			elif count==6 :
-				if '0' in word:
-					broken=True
-
-				if broken==True: 
-					counton=False
-					count=0
-					destination.write(hold + '\n')
-					print(hold)
-					broken=False
-				else:
+				elif check==True :
+					check=False
+					destination.write(word + ' ')
 					print(word)
-					destination.write(word + '\n')
-					counton=False
-					count=0
-			if counton==True:
-				count +=1
+				elif count==5 :
+					if ',' in word:
+						broken=True
+
+				elif count==6 :
+					if '0' in word:
+						broken=True
+
+					if broken==True: 
+						counton=False
+						count=0
+						destination.write(hold + '\n')
+						print(hold)
+						broken=False
+					else:
+						print(word)
+						destination.write(word + '\n')
+						counton=False
+						count=0
+				if counton==True:
+					count +=1
 if __name__=='__main__':
 	filelayout()
 				
